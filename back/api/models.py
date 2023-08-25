2bcc8976ed71cbe09c4044f1015d3f772918e4f4
sheverdin-mikhail <msheverdin648@gmail.com>
2023-08-25 19:09:14 +0300

    writed models for project

--- back/api/models.py
+++ back/api/models.py
@@ -2,15 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
-
 
 class User(AbstractUser):
-    pass
+    """Общая модель пользователя"""
+    fio = models.CharField(("ФИО"), max_length=255)
+    phone_number = models.CharField(("Номер телефона"), max_length=20)
+
 
 
 class LegalEntity(models.Model):
-
+    """Юр. лицо"""
     name = models.CharField(("Наименование юридиеского лица"), max_length=100)
 
     class Meta:
@@ -23,17 +24,36 @@ class LegalEntity(models.Model):
     
 
 class CompanyMember(models.Model):
+    """Работник компании SWATZKY"""
+
+    ROLES = (
+        ('dispatcher', 'Диспетчер'),
+        ('performer', 'Исполнитель'),
+        ('dispatcher_performer', 'Диспетчер/Исполнитель'),
+    )
+
+    GROUPS = (
+        ('S1', 'Управляющий Объекта'),
+        ('S2', 'Руководитель Отдела Управления'),
+        ('S3', 'Исполнительный директор'),
+        ('S4', 'Руководство'),
+        ('S5', 'Администратор системы'),
+        ('S6', 'Разработчик системы'),
+    )
 
     user = models.OneToOneField(User, verbose_name=("Пользователь"), on_delete=models.CASCADE, related_name='performer')
+    role = models.CharField(choices=ROLES, default='user', max_length=20, verbose_name='Роль пользователя')
+    group = models.CharField(choices=GROUPS, default='S1', max_length=20, verbose_name='Группа пользователя')
 
     class Meta:
-        verbose_name = "Исполнитель"
-        verbose_name_plural = "Исполнители"
+        verbose_name = "Сотрудник SWATZKY"
+        verbose_name_plural = "Сотрудники SWATZKY"
 
     def __str__(self):
-        return f"{self.user.id}: {self.user.first_name} {self.user.last_name}" 
+        return f"{self.user.id}: {self.user.fio}" 
     
 class WorkObjectsGroup(models.Model):
+    """Группа рабочих объектов"""
 
     name = models.CharField(("Наименование группы"), max_length=50)
     code = models.CharField(("Код объекта"), max_length=50)
@@ -48,6 +68,7 @@ class WorkObjectsGroup(models.Model):
         return self.name
 
 class WorkObject(models.Model):
+    """Рабочий объект"""
 
     name = models.CharField(("Наименование группы"), max_length=50)
     code = models.CharField(("Код объекта"), max_length=50)
@@ -64,27 +85,34 @@ class WorkObject(models.Model):
 
 
 
-
 class Client(models.Model):
+    """Заказчик"""
 
+    user = models.OneToOneField(User, verbose_name=("Пользователь"), on_delete=models.CASCADE)
     legal_entity = models.ForeignKey(LegalEntity, verbose_name=("Юридическое лицо"), on_delete=models.CASCADE)
+    work_objects_group = models.ManyToManyField(WorkObjectsGroup, verbose_name=("Группы рабочих объектов"), related_name='client')
+    work_object = models.ForeignKey(
+        WorkObject,
+        verbose_name=("Рабоий объект"), 
+        on_delete=models.CASCADE, 
+        related_name='client', 
+        blank=True, 
+        null=True
+    )
 
     class Meta:
         verbose_name = "Закачик/Арендатор"
         verbose_name_plural = "Заказчики/Арендаторы"
 
     def __str__(self):
-        return self.name
+        return f"{self.id}: { self.legal_entity } | {self.user.fio}"
 
     
     
-    
-
-
-
-
 
 class Application(models.Model):
+
+    """Заявка на работу"""
 
     title = models.CharField(("Заголовок заявки"), max_length=50)
     subject = models.CharField(("Предмет запроса"), max_length=300)
@@ -105,3 +133,26 @@ class Application(models.Model):
 
     
 
+class Report(models.Model):
+    """Отчет"""
+    
+
+    client = models.ForeignKey(Client, verbose_name=("Заказчик в рамках которого создается отчет"), on_delete=models.CASCADE)
+    company_member = models.ForeignKey(CompanyMember, verbose_name=("Сотрудник, создавший отчет"), on_delete=models.CASCADE)
+
+    founded_apllications = models.ManyToManyField(Application, verbose_name=("Найденные заявки"))
+
+    period_start = models.DateField(("Период поиска заявок от"), auto_now=False, auto_now_add=False)
+    period_end = models.DateField(("Период поиска заявок до"), auto_now=False, auto_now_add=False)
+
+    created_at = models.DateField(("Дата создания заявки"), auto_now=False, auto_now_add=True)
+    updated_at = models.DateField(("Последняя дата изменения"), auto_now=True, auto_now_add=False)
+
+    class Meta:
+        verbose_name = ("Отчет")
+        verbose_name_plural = ("Отчеты")
+
+    def __str__(self):
+        return self.name
+
+   
