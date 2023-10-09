c3c66f020cb9f92d273892d54cd60cced47d1aed
sheverdin-mikhail <msheverdin648@gmail.com>
2023-10-09 15:35:07 +0300

    created prod configs

--- front/src/shared/api/api.ts
+++ front/src/shared/api/api.ts
@@ -2,10 +2,10 @@ import axios  from 'axios';
 import { USER_LOCALSTORAGE_TOKENS } from 'shared/const/localStorage';
 
 const tokens = localStorage.getItem(USER_LOCALSTORAGE_TOKENS) || ''
-
+const isDev = false 
 
 export const $api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: isDev ? 'http://localhost:8000' : 'http://77.223.126.236',
     headers: {
         Authorization: `Bearer ${tokens && JSON.parse(tokens).access}`
     }
