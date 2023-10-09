14355ff971c8b56ad8cbedb733fcf9f0a8d362df
sheverdin-mikhail <msheverdin648@gmail.com>
2023-10-09 22:05:36 +0300

    return docker-compose.dev.yml to root folder

--- front/src/shared/api/api.ts
+++ front/src/shared/api/api.ts
@@ -2,7 +2,7 @@ import axios  from 'axios';
 import { USER_LOCALSTORAGE_TOKENS } from 'shared/const/localStorage';
 
 const tokens = localStorage.getItem(USER_LOCALSTORAGE_TOKENS) || ''
-const isDev = false 
+const isDev = true 
 
 export const $api = axios.create({
     baseURL: isDev ? 'http://localhost:8000' : 'http://77.223.126.236',
