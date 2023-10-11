5521471386febfffa33512f99ef99a9338b0984f
aldreamex <mukhitov-2000@mail.com>
2023-10-11 11:43:19 +0300

    task UserDetailView

--- back/api/urls.py
+++ back/api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('users/me/', views.AuthUserView.as_view()),
+    path('users/<int:user_id>/', views.UserDetailView.as_view()),
     path('applications/', views.ApplicationListView.as_view()),
     path('applications/create/', views.ApplicationCreateView.as_view()),
     path('applications/<int:pk>/', views.ApplicationDetailView.as_view()),
