47e9c42eec359bf6a8241b3dda67d77e955cb5a5
sheverdin-mikhail <msheverdin648@gmail.com>
2023-10-12 09:59:10 +0300

    retrun access_token_lifetime to 90

--- back/config/settings.py
+++ back/config/settings.py
@@ -186,7 +186,7 @@ SIMPLE_JWT = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=90),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
