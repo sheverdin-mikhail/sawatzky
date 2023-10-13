a7e2d0dab23a19baae33b8d8aaec9e730a345853
aldreamex <139506980+aldreamex@users.noreply.github.com>
2023-10-13 10:30:57 +0300

    Api dev (#17)
    * test commit
    
    * ...
    
    * ....
    
    * ....
    
    * remove .idea
    
    * add api interceptors for refreshing (#13)
    
    * task Client
    
    * ref permission_classes - Client task
    
    * ref Serializers + Views - Client task
    
    * ref Views - Client task
    
    ---------
    
    Co-authored-by: sheverdin-mikhail <msheverdin648@gmail.com>
    Co-authored-by: aldreamex <mukhitov-2000@mail.com>

--- back/api/serializers.py
+++ back/api/serializers.py
@@ -2,20 +2,45 @@ from rest_framework.serializers import ModelSerializer
 from .models import (
     User,
     Employee,
-    Application
+    Application,
+    Client,
+    LegalEntity,
+    WorkObjectsGroup,
+    WorkObject,
 )
 
 
 
 class EmployeeSerializer(ModelSerializer):
-    #Сериализатор модели расширяющей профиль пользователя
+    # Сериализатор модели расширяющей профиль пользователя
     class Meta:
         model = Employee
         fields = '__all__'
 
 
+class LegalEntitySerializer(ModelSerializer):
+    # Сериализатор модели LegalEntity, расширяющей профиль клиента
+    class Meta:
+        model = LegalEntity
+        fields = '__all__'
+
+
+class WorkObjectSerializer(ModelSerializer):
+    # Сериализатор модели WorkObject, расширяющей профиль клиента
+    class Meta:
+        model = WorkObject
+        fields = '__all__'
+
+
+class WorkObjectsGroupSerializer(ModelSerializer):
+    # Сериализатор модели WorkObjectsGroup, расширяющей профиль клиента
+    class Meta:
+        model = WorkObjectsGroup
+        fields = '__all__'
+
+
 class UserSerializer(ModelSerializer):
-    #Сериализатор модели пользователя для отображения данных о нем
+    # Сериализатор модели пользователя для отображения данных о нем
     employee = EmployeeSerializer(read_only=True, many=False)
 
     class Meta:
@@ -24,7 +49,7 @@ class UserSerializer(ModelSerializer):
 
 
 class UserSerializerWithoutEmployee(ModelSerializer):
-    #Сериализатор модели пользователя без поля Employee
+    # Сериализатор модели пользователя без поля Employee
 
     class Meta:
         model = User
@@ -32,7 +57,7 @@ class UserSerializerWithoutEmployee(ModelSerializer):
 
 
 class EmployeeSerializer(ModelSerializer):
-    #Сериализатор для сотрудника с расширенным полем юзера
+    # Сериализатор для сотрудника с расширенным полем юзера
     user = UserSerializerWithoutEmployee(read_only=True, many=False)
 
     class Meta:
@@ -41,7 +66,7 @@ class EmployeeSerializer(ModelSerializer):
 
 
 class ApplicationWithCreatorSerializer(ModelSerializer):
-    #Сериализаатор для вывода списка заявок с расширенным полем creator
+    # Сериализаатор для вывода списка заявок с расширенным полем creator
     creator =  EmployeeSerializer(read_only=True, many=False)
 
     class Meta:
@@ -50,7 +75,7 @@ class ApplicationWithCreatorSerializer(ModelSerializer):
 
 
 class ApplicationSerializer(ModelSerializer):
-    #Сериализаатор для создания/удаления заявки
+    # Сериализаатор для создания/удаления/обновления заявки
 
     class Meta:
         model = Application
@@ -58,5 +83,24 @@ class ApplicationSerializer(ModelSerializer):
         fields = '__all__'
 
 
+class ClientWithCLWWSerializers(ModelSerializer):
+    # Сериализатор для вывода списка клиентов с расширенным полем creator, legalEntity, workObject, workObjectsGroup
+    creator = EmployeeSerializer(read_only=True, many=False)
+    legalEntity = LegalEntitySerializer(read_only=True, many=False)
+    workObject = WorkObjectSerializer(read_only=True, many=False)
+    workObjectsGroup = WorkObjectsGroupSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+
+class ClientSerializers(ModelSerializer):
+    # Сериализатор для создания/удаления/обновления клиента
+
+    class Meta:
+        model = Client
+        many = False
+        fields = '__all__'
 
 
