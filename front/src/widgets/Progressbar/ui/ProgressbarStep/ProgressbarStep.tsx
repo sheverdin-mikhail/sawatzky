91c46afa4885482fc13c7a2258d594664db1f032
Kromus22 <kromus2@gmail.com>
2023-11-02 23:11:22 +0500

    +fix lines in ProgressbarStep

--- front/src/widgets/Progressbar/ui/ProgressbarStep/ProgressbarStep.tsx
+++ front/src/widgets/Progressbar/ui/ProgressbarStep/ProgressbarStep.tsx
@@ -13,18 +13,21 @@ export const ProgressbarStep: React.FC<ProgressStepProps> = (props) => {
   const { id, title, step } = props;
 
   const mods: Mods = {
-    [cls.done_start]: (+id <= step && +id === 1) || +id < step,
-    [cls.done_end]: (+id === step && step !== 7) && +id !== 1,
-    [cls.done_cancel]: (+id <= step || step === 7) && +id !== 1,
-    [cls.step_start]: +id > step && +id === 1,
-    [cls.step_end]: +id > step && +id !== 7,
-    [cls.step_cancel]: +id > step && +id !== 1,
     [cls.done]: +id <= step,
     [cls.step]: +id > step,
   }
 
+  const modsItem: Mods = {
+    [cls.done_start]: (+id <= step && +id === 1) || +id < step,
+    [cls.done_end]: (+id === step && step !== 7) && +id !== 1,
+    [cls.done_cancel]: (+id <= step || step === 7) && +id !== 1,
+    [cls.step_start]: +id >= step && +id === 1,
+    [cls.step_end]: +id > step && +id !== 7,
+    [cls.step_cancel]: +id > step && +id !== 1,
+  }
+
   return (
-    <li className={cls.item} id={id}>
+    <li className={classNames(cls.item, modsItem, [])} id={id}>
       <div className={classNames('', mods, [])}>
         {+id > step ? id : <DoneIcon />}
       </div>
