cd27a65cf2d33d5dbca2bb769fe6d3232b65a4dd
sheverdin-mikhail <msheverdin648@gmail.com>
2023-12-02 04:00:17 +0400

    add loader for ApplicationsPage

--- front/src/pages/ApplicationsPage/model/selectors/applicationsPageSelectors.ts
+++ front/src/pages/ApplicationsPage/model/selectors/applicationsPageSelectors.ts
@@ -5,6 +5,7 @@ export const getAllIsChecked = (state: StateSchema) => state.applicationsPage?.a
 export const getCheckedItems = (state: StateSchema) => state.applicationsPage?.checkedItems;
 export const getModalIsOpen = (state: StateSchema) => state.applicationsPage?.modalIsOpen;
 export const getPageInit = (state: StateSchema) => state.applicationsPage?._init;
+export const getApplicationIsLoading = (state: StateSchema) => state.applicationsPage?.isLoading;
 
 export const getRowItemIsChecked = (id: string) => createSelector(
   [getCheckedItems],
