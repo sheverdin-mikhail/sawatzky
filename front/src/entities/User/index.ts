11a2e5f693ab14de151aa93019156260e5b20a59
sheverdin-mikhail <msheverdin648@gmail.com>
2023-12-05 20:51:47 +0400

    create userRoles

--- front/src/entities/User/index.ts
+++ front/src/entities/User/index.ts
@@ -5,3 +5,10 @@ export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthDa
 export { getUserInited } from './model/selectors/getUserInited/getUserInited';
 
 export { userActions, userReducer } from './model/slice/userSlice';
+
+export {
+  getUserData,
+  getUserEmployee,
+  getUserRole,
+  isSawatzky,
+} from './model/selectors/getUserRole/getUserRole';
