965357086196b731dd3a72fce97450253d434913
Kromus22 <kromus2@gmail.com>
2023-12-09 00:10:34 +0500

    +ForbiddenPage

--- front/src/pages/ForbiddenPage/ui/ForbiddenPage/ForbiddenPage.tsx
+++ front/src/pages/ForbiddenPage/ui/ForbiddenPage/ForbiddenPage.tsx
@@ -1,15 +1,20 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import { ReactComponent as ForbiddenIcon } from 'shared/assets/icons/forbidden-icon.svg';
+import cls from './ForbiddenPage.module.scss';
 
 interface ForbiddenPageProps {
-    className?: string;
+  className?: string;
 }
 
 const ForbiddenPage: React.FC<ForbiddenPageProps> = (props) => {
   const { className } = props;
 
   return (
-    <div className={classNames('', {}, [className])}>
-      Недостаточно прав для доступа к этой странице
+    <div className={classNames(cls.forbidden, {}, [className])}>
+      <div className={cls.form}>
+        <ForbiddenIcon />
+        <p className={cls.text}>Недостаточно прав доступа!</p>
+      </div>
     </div>
   );
 };
