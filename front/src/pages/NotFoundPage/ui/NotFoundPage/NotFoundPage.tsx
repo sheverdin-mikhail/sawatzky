8101ad1cfbfed946ec0911424007c1c129469ddd
Kromus22 <kromus2@gmail.com>
2023-12-09 00:15:13 +0500

    +NotFoundPage

--- front/src/pages/NotFoundPage/ui/NotFoundPage/NotFoundPage.tsx
+++ front/src/pages/NotFoundPage/ui/NotFoundPage/NotFoundPage.tsx
@@ -1,16 +1,17 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-// import cls from './NotFoundPage.module.scss';
+import cls from './NotFoundPage.module.scss';
 
 interface NotFoundPageProps {
-    className?: string;
+  className?: string;
 }
 
 const NotFoundPage: React.FC<NotFoundPageProps> = (props) => {
   const { className } = props;
 
   return (
-    <div className={classNames('', {}, [className])}>
-      Страница не найдена
+    <div className={classNames(cls.notFound, {}, [className])}>
+      <p className={cls.number}>404</p>
+      <p className={cls.text}>Ой... Мы не можем найти страницу!</p>
     </div>
   );
 };
