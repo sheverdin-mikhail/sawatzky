6577d4351ee0cc73efc6a016121e3d57fc6af6b5
Kromus22 <kromus2@gmail.com>
2023-12-15 21:16:51 +0500

    +ReportsPage WIP

--- front/src/pages/ReportsPage/ui/ReportsPageContent/ReportsPageContent.tsx
+++ front/src/pages/ReportsPage/ui/ReportsPageContent/ReportsPageContent.tsx
@@ -1,6 +1,12 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Title } from 'shared/ui/Title/Title';
+import { Button, ButtonThemes } from 'shared/ui/Button/Button';
+import { ReactComponent as OrderLogo } from 'shared/assets/icons/order-icon.svg';
+import { ReactComponent as AddLogo } from 'shared/assets/icons/add-icon.svg';
+import { ReactComponent as DeleteLogo } from 'shared/assets/icons/delete-icon.svg';
+import { ReactComponent as DeleteFileLogo } from 'shared/assets/icons/del-file-icon.svg';
 import cls from './ReportsPageContent.module.scss';
+import { ReportsList } from '../ReportsList/ReportsList';
 
 interface ReportsPageContentProps {
   className?: string;
@@ -11,7 +17,14 @@ export const ReportsPageContent: React.FC<ReportsPageContentProps> = (props) => 
 
   return (
     <div className={classNames(cls.reportsPageContent, {}, [className])}>
-      <Title>Отчеты</Title>
+      <Title className={cls.title}>Отчеты</Title>
+      <div className={cls.navigation}>
+        <Button className={cls.btn} theme={ButtonThemes.ICON}> <OrderLogo /></Button>
+        <Button className={cls.btn} theme={ButtonThemes.ICON} helpInfo="добавить отчёт"> <AddLogo /></Button>
+        <Button className={cls.btn} theme={ButtonThemes.ICON} helpInfo="удалить отчёт"> <DeleteLogo /></Button>
+        <Button className={cls.btn} theme={ButtonThemes.ICON}> <DeleteFileLogo /></Button>
+      </div>
+      <ReportsList />
     </div>
   );
 };
