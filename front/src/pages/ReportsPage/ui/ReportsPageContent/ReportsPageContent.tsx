957ab80c3181ce6fced5f9c409562a1e34c659e7
Kromus22 <kromus2@gmail.com>
2023-12-16 17:33:34 +0500

    +AddReportModal

--- front/src/pages/ReportsPage/ui/ReportsPageContent/ReportsPageContent.tsx
+++ front/src/pages/ReportsPage/ui/ReportsPageContent/ReportsPageContent.tsx
@@ -5,6 +5,8 @@ import { ReactComponent as OrderLogo } from 'shared/assets/icons/order-icon.svg'
 import { ReactComponent as AddLogo } from 'shared/assets/icons/add-icon.svg';
 import { ReactComponent as DeleteLogo } from 'shared/assets/icons/delete-icon.svg';
 import { ReactComponent as DeleteFileLogo } from 'shared/assets/icons/del-file-icon.svg';
+import { AddReportModal } from 'features/AddReport';
+import { useCallback, useState } from 'react';
 import cls from './ReportsPageContent.module.scss';
 import { ReportsList } from '../ReportsList/ReportsList';
 
@@ -15,16 +17,23 @@ interface ReportsPageContentProps {
 export const ReportsPageContent: React.FC<ReportsPageContentProps> = (props) => {
   const { className } = props;
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onReportModalCloseHandler = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
   return (
     <div className={classNames(cls.reportsPageContent, {}, [className])}>
       <Title className={cls.title}>Отчеты</Title>
       <div className={cls.navigation}>
         <Button className={cls.btn} theme={ButtonThemes.ICON}> <OrderLogo /></Button>
-        <Button className={cls.btn} theme={ButtonThemes.ICON} helpInfo="добавить отчёт"> <AddLogo /></Button>
+        <Button className={cls.btn} theme={ButtonThemes.ICON} helpInfo="добавить отчёт" onClick={() => setIsOpen(true)}> <AddLogo /></Button>
         <Button className={cls.btn} theme={ButtonThemes.ICON} helpInfo="удалить отчёт"> <DeleteLogo /></Button>
         <Button className={cls.btn} theme={ButtonThemes.ICON}> <DeleteFileLogo /></Button>
       </div>
       <ReportsList />
+      <AddReportModal isOpen={isOpen} onClose={onReportModalCloseHandler} />
     </div>
   );
 };
