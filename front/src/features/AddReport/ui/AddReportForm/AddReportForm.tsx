00e640f647b0aff9cca0ad9266f2e85d1e0b0b87
Kromus22 <kromus2@gmail.com>
2023-12-19 20:56:54 +0500

    try to add options for ReportForm Selects

--- front/src/features/AddReport/ui/AddReportForm/AddReportForm.tsx
+++ front/src/features/AddReport/ui/AddReportForm/AddReportForm.tsx
@@ -1,8 +1,11 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Text, TextAlign } from 'shared/ui/Text/Text';
 import { Input } from 'shared/ui/Input/Input';
-import { Select } from 'shared/ui/Select/Select';
+import { Select, SelectOptionType } from 'shared/ui/Select/Select';
 import { Button, ButtonThemes } from 'shared/ui/Button/Button';
+import { useSelector } from 'react-redux';
+import { getWorkObjectGroup } from 'entities/WorkObjectGroup';
+import { useMemo } from 'react';
 import cls from './AddReportForm.module.scss';
 
 interface AddReportFormProps {
@@ -12,15 +15,19 @@ interface AddReportFormProps {
 export const AddReportForm: React.FC<AddReportFormProps> = (props) => {
   const { className } = props;
 
+  const workObjectsGroups = useSelector(getWorkObjectGroup.selectAll);
+
+  const workObjectGroupOptions: SelectOptionType[] = workObjectsGroups.map((item) => ({ value: item.id, text: item.name }));
+
   return (
     <div className={classNames(cls.addReportForm, {}, [className])}>
       <Text className={cls.title} title="Создать новый отчет" textAlign={TextAlign.CENTER} />
 
       <Input className={cls.input} placeholder="Период" />
-      <Select className={classNames((cls.input), {}, [cls.select])} placeholder="Группа объектов" />
-      <Select className={classNames((cls.input), {}, [cls.select])} placeholder="Объекты" />
-      <Select className={classNames((cls.input), {}, [cls.select])} placeholder="Контрагенты" />
-      <Select className={classNames((cls.input), {}, [cls.select])} placeholder="Заказчики" />
+      <Select className={cls.input} placeholder="Группа объектов" options={workObjectGroupOptions} />
+      <Select className={cls.input} placeholder="Объекты" />
+      <Select className={cls.input} placeholder="Контрагенты" />
+      <Select className={cls.input} placeholder="Заказчики" />
 
       <Button className={cls.btn} theme={ButtonThemes.BLUE_SOLID}>Создать</Button>
     </div>
