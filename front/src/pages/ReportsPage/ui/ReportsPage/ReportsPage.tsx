3da609caabc2cc7ca6528a772594034b34041a9e
Kromus22 <kromus2@gmail.com>
2023-12-20 23:29:27 +0500

    +options for workGroups in ReportsPage modal

--- front/src/pages/ReportsPage/ui/ReportsPage/ReportsPage.tsx
+++ front/src/pages/ReportsPage/ui/ReportsPage/ReportsPage.tsx
@@ -1,16 +1,32 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { fetchWorkObjectGroupList, workObjectGroupReducer } from 'entities/WorkObjectGroup';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useEffect } from 'react';
 import { ReportsPageContent } from '../ReportsPageContent/ReportsPageContent';
 
 interface ReportsPageProps {
   className?: string;
 }
 
+const reducers: ReducersList = {
+  workObjectGroup: workObjectGroupReducer,
+};
+
 export const ReportsPage: React.FC<ReportsPageProps> = (props) => {
   const { className } = props;
 
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchWorkObjectGroupList());
+  }, [dispatch]);
+
   return (
-    <div className={classNames('', {}, [className ?? ''])}>
-      <ReportsPageContent />
-    </div>
+    <DynamicModuleLoader reducers={reducers}>
+      <div className={classNames('', {}, [className ?? ''])}>
+        <ReportsPageContent />
+      </div>
+    </DynamicModuleLoader>
   );
 };
